c9bd2405f2c23b5a2e05f1a11ef1e603b73cb782
Adrian Silva <grsadrian01@gmail.com>
2025-07-19 13:38:39 -0300

    feat(ui): style the menu options

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const sketchContainer = document.querySelector("#sketch-container")
+const sketchContainer = document.querySelector("#sketch-container");
+const btns = document.querySelectorAll("button");
 
-for(let i = 0; i < 100; i++){
-    let div = document.createElement('div')
-    div.classList.add('sketch-block')
-    div.style.width = `${720/10}px`
-    div.style.height = `${720/10 }px`
-    sketchContainer.appendChild(div)
+function setActiveClass(event) {
+  if (event.currentTarget.getAttribute("id") != "clear") {
+    btns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    event.currentTarget.classList.add("active");
+  }
+}
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", setActiveClass);
+});
+
+for (let i = 0; i < 256; i++) {
+  let div = document.createElement("div");
+  div.classList.add("sketch-block");
+  div.style.width = `${sketchContainer.clientWidth / 16}px`;
+  div.style.height = `${sketchContainer.clientHeight / 16}px`;
+  sketchContainer.appendChild(div);
 }
