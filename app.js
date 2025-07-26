b4a3b6e3c3cec62a7f131b7f57f6608ee12d1166
Adrian Silva <grsadrian01@gmail.com>
2025-07-26 12:34:12 -0300

    feat(app): add functions to the buttons of menu

--- app.js
+++ app.js
@@ -3,23 +3,30 @@ const menuContainer = document.querySelector(".menu-container");
 const colorSelector = document.querySelector("#color-selector");
 const sizeGridSelector = document.querySelector("#size-selector");
 const displaySizeGridValue = document.querySelector("#size-value");
-const btns = document.querySelectorAll("button");
+const buttons = document.querySelectorAll("button");
 let modeValue = "color-mode";
 let colorValue = "#1921ff";
 let sizeGridValue = 16;
 let mouseDown;
+let rainbowModeActive = false;
 
-makeGrid(sizeGridValue)
+makeGrid(sizeGridValue);
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", setActiveClass);
-});
+menuContainer.addEventListener("click", handleButtons);
 
 sizeGridSelector.addEventListener("mousemove", setSizeGridValue);
 sizeGridSelector.addEventListener("change", changeSizeGrid);
 colorSelector.addEventListener("input", setColorValue);
-sketchContainer.addEventListener("mousedown", () => mouseDown = true);
-sketchContainer.addEventListener("mouseup", () => mouseDown = false)
+sketchContainer.addEventListener("mousedown", () => (mouseDown = true));
+sketchContainer.addEventListener("mouseup", () => (mouseDown = false));
+sketchContainer.addEventListener("mouseleave", () => (mouseDown = false));
+
+function generateRainbowColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
 
 function setColorValue(event) {
   colorValue = event.target.value;
@@ -32,7 +39,7 @@ function setModeValue(value) {
 function makeGrid(size) {
   const blockWidth = sketchContainer.clientWidth / size;
   const blockHeight = sketchContainer.clientHeight / size;
-  reloadGrid();
+
   for (let i = 0; i < size ** 2; i++) {
     const block = document.createElement("div");
     block.classList.add("sketch-block");
@@ -40,6 +47,7 @@ function makeGrid(size) {
     block.style.height = `${blockHeight}px`;
     sketchContainer.appendChild(block);
     block.addEventListener("mousemove", changeColor);
+    block.addEventListener("click", changeColor);
   }
 }
 
@@ -48,23 +56,51 @@ function reloadGrid() {
 }
 
 function changeColor(event) {
-  if (mouseDown) {
-    console.log(event.currentTarget)
-    event.target.style.background = colorValue;
+  if (mouseDown || event.type === "click") {
+    if (rainbowModeActive) {
+      event.target.style.background = generateRainbowColor();
+    } else {
+      event.target.style.background = colorValue;
+    }
   }
 }
 
 function changeSizeGrid(event) {
   sizeGridValue = event.currentTarget.value;
+  reloadGrid();
   makeGrid(sizeGridValue);
 }
 
-function setActiveClass(event) {
-  if (event.currentTarget.getAttribute("id") != "clear") {
-    btns.forEach((btn) => {
-      btn.classList.remove("active");
+function handleButtons(event) {
+  function removeActiveClass() {
+    buttons.forEach((button) => {
+      button.classList.remove("active");
     });
-    event.currentTarget.classList.add("active");
+  }
+
+  setModeValue(event.target.getAttribute("id"));
+  switch (modeValue) {
+    case "clear":
+      reloadGrid();
+      makeGrid(sizeGridValue);
+      break;
+    case "erase-mode":
+      removeActiveClass();
+      rainbowModeActive = false;
+      event.target.classList.add("active");
+      colorValue = "#FFF";
+      break;
+    case "color-mode":
+      removeActiveClass();
+      rainbowModeActive = false;
+      event.target.classList.add("active");
+      colorValue = colorSelector.value;
+      break;
+    case "rainbow-mode":
+      removeActiveClass();
+      event.target.classList.add("active");
+      rainbowModeActive = true;
+      break;
   }
 }
 
